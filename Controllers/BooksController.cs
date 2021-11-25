5c52820b4e45f643ed11274228fa67c3cee0e89b
Ruban Anderson <aruban@solutions.ht>
2021-11-25 14:50:12 -0500

    chapter 2 concepts

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Services;
+using my_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BookService _bookService;
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet("books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _bookService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("books/{id}")]
+        public IActionResult GetBook(int id)
+        {
+            var book = _bookService.GetBook(id);
+            return Ok(book);
+        }
+
+        [HttpPost("books")]
+        public IActionResult AddBook([FromBody]BookVM book)
+        {
+            _bookService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("books/{id}")]
+        public IActionResult UpdateBook(int id, [FromBody]BookVM book)
+        {
+            var updateBook = _bookService.UpdateBook(id, book);
+            return Ok(updateBook);
+        }
+
+        [HttpDelete("books/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _bookService.DeleteBook(id);
+            return Ok();
+        }
+    }
+}
